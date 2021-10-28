02d9563a74b66a9e582f59b4626b56a159cc0f88
Saravanan T <saravanansaravana722@gmail.com>
2021-10-28 11:49:45 +0530

    Alignment

--- Genric.java
+++ Genric.java
@@ -1,7 +1,7 @@
 
 public class Genric {
     public static void main(String[] args) {
-        GenricExample1<String,String> x = new GenricExample1<String,String>("aaaa","aaaaa");
+        GenricExample1<String, String> x = new GenricExample1<String, String>("aaaa", "aaaaa");
         System.out.println(x.getShow());
         System.out.println(x.show1());
 
@@ -16,26 +16,24 @@ class GenricExample<T> {
     T t;
 
     public GenricExample(T a) {
-
         t = a;
-
     }
 
-      T getShow() {
-         return t;
-        
+    T getShow() {
+        return t;
+
     }
 }
 
-class GenricExample1<T,d> extends GenricExample<T> {
+class GenricExample1<T, d> extends GenricExample<T> {
     T d;
 
-    public GenricExample1(T a,T Type1) {
+    public GenricExample1(T a, T Type1) {
         super(a);
         this.d = Type1;
     }
 
-     T show1() {
+    T show1() {
         return d;
-    } 
+    }
 }
